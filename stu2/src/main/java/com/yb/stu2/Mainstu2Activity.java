471a6252d33be0e3cbd7ce8092e619c176dde791
Yousef Ballan <ballan_yousef@yahoo.com>
2019-12-14 22:21:54 +0200

    message 1

--- stu2/src/main/java/com/yb/stu2/Mainstu2Activity.java
+++ stu2/src/main/java/com/yb/stu2/Mainstu2Activity.java
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Mainstu2Activity extends AppCompatActivity {
-
+    int x;
+    int y;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainstu2);
+
     }
 }
